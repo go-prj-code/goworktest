e610690d2e04609349e3c4bb49918ba46f3b968d
william.yangshun <2095686947@qq.com>
2022-10-16 04:33:24 +0800

    modify

--- common.go
+++ common.go
@@ -5,3 +5,7 @@ import "fmt"
 func PrintStr(str string, num int) string {
 	return fmt.Sprintf("project C %s_%d", str, num)
 }
+
+func ForGwsprjTest() string {
+	return "hi go workspace test prj1"
+}
